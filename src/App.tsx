99e945008342fe87644330934f7c43245b63d8a9
Leandro <mellyleo17@gmail.com>
2025-10-12 01:06:42 -0300

    arregle-pre-render

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,3 @@
-// import React from "react";
-// import "./App.css";
-// import "leaflet/dist/leaflet.css";
-// import { MapProvider } from "./context/MapContext";
-// import { MapView } from "./components/map/MapView";
-// import { useMapHash } from "./hooks/useMapHash";
-// import { ErrorBoundary } from "./components/ErrorBoundary";
-
-// function App(): React.JSX.Element {
-//   const { getInitialMapState } = useMapHash();
-//   const initialMapState = getInitialMapState();
-
-//   return (
-//     <ErrorBoundary>
-//       <MapProvider initialState={initialMapState}>
-//         <MapView />
-//       </MapProvider>
-//     </ErrorBoundary>
-//   );
-// }
-
-// export default App;
 import React from "react";
 import "./App.css";
 import "leaflet/dist/leaflet.css";
